689f14151a0d9da29d087062ab3d2455cae79edb
YunLiang <YunLiang@LAPTOP-KJ661URH.mshome.net>
2018-01-30 17:48:18 +0800

    account-data project initial

---
+++ src/main/java/io/pivotal/microservices/services/Main.java
@@ -0,0 +1,18 @@
+package io.pivotal.microservices.services;
+
+import io.pivotal.microservices.services.accounts.AccountsServer;
+
+/**
+ * Allow the servers to be invoked from the command-line. The jar is built with
+ * this as the <code>Main-Class</code> in the jar's <code>MANIFEST.MF</code>.
+ * 
+ */
+public class Main {
+
+	public static void main(String[] args) {
+
+		AccountsServer.main(args);
+
+	}
+ 
+}
